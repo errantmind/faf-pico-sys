4463f3a1ebcf323d7eff12e9386a9c83aefa924b
James Bates <jmbates@gmail.com>
2021-12-15 12:42:51 -0600

    Updated build.rs, removed unnecessary option

--- build.rs
+++ build.rs
@@ -1,10 +1,12 @@
 fn main() {
     cc::Build::new()
         .file("extern/picohttpparser/picohttpparser.c")
-        .opt_level_str(&"fast")
+        .opt_level_str("fast")
         .flag("-funroll-loops")
-        .flag("-msse4")
         .flag("-flto=thin")
         .flag("-march=native")
+        //.flag("-fwhole-program-vtables")
+        //.flag("-mllvm")
+        //.flag("--enable-loop-distribute")
         .compile("libpicohttpparser.a");
 }
